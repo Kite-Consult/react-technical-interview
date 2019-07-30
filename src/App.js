daddb41ca6022e1ba1132c535e24902325924f85
Salvador Verde Rodriguez <salvador.verde@kite-consult.de>
2019-07-30 12:31:10 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import HomeView from "./components/HomeView";
 
-const App = props => <HomeView />;
+const App = props => (
+  <Router>
+    <Route path="/" exact component={HomeView} />
+  </Router>
+);
 
 export default App;
